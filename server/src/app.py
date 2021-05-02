a675bc734f4a8368dd30913fa5a8ad2bf18a3f91
Keval Botadra <keval.botadra@gmail.com>
2021-05-02 14:23:47 -0400

    added profile clustering functionality

--- server/src/app.py
+++ server/src/app.py
@@ -19,6 +19,47 @@ import pickle
 from gpt import ask
 
 
+def process_text(text, stem=True):
+    texts = text.translate(str.maketrans('','',string.punctuation))
+    tokens = word_tokenize(texts)
+
+    if not stem:
+        return tokens
+    else:
+        stemmer = PorterStemmer()
+        tokens = [stemmer.stem(t) for t in tokens]
+
+def cluster_texts(texts, clusters):
+    print(texts)
+    vectorizer = TfidfVectorizer(stop_words=stopwords.words('english'))
+    tfidf = vectorizer.fit_transform(texts)
+    print(tfidf)
+    kmeans_model = KMeans(n_clusters=clusters)
+    kmeans_model.fit(tfidf)
+
+    clustering = collections.defaultdict(list)
+
+    for index, label in enumerate(kmeans_model.labels_):
+        clustering[label].append(index)
+
+    return clustering
+
+def grouping():
+  with open("bios.json", "r") as f:
+    request_json = json.load(f)
+
+  bios = request_json["bios"]
+
+  # process_text(bios)
+
+  clusters = len(bios) / 2
+  
+
+  clusters  = cluster_texts(bios, int(clusters))
+
+  pprint(dict(clusters))
+
+
 app = Flask(__name__)
 
 nltk.download("punkt")
@@ -147,6 +188,55 @@ AI: Yes, I can definitely help you. What's on your mind?
     
     else:
         return {"code" : 401, "error" : "request missing arguments"}
+
+
+@app.route("/group-profiles", methods=["POST"])
+def grouping():
+    request_json = flask.request.json
+    
+    user = request_json["user"]
+    all_users = request_json["all_users"]
+
+    ids = []
+    bios = []
+
+    for user in all_users:
+        for id, bio in user:
+        ids.append(id)
+        bios.append(bio)
+    
+    for id, bio in user:
+        ids.append(id)
+        bios.append(bio)
+        to_predict = id  
+    # process_text(bios)
+
+    clusters = len(bios) / 2
+
+
+    clusters  = cluster_texts(bios, int(clusters))
+
+    clusters = dict(clusters)
+
+
+    for item in clusters:
+        for value in clusters[item]:
+        if value == to_predict:
+            to_predict_key = item
+        else:
+            pass
+
+    profile_index = random.choice(clusters[to_predict_key])
+
+    while profile_index == to_predict:
+        profile_index = random.choice(clusters[to_predict_key])
+
+
+    profile_id = ids[profile_index]
+
+
+    return {"id" : profile_id}
+
 
 
 
